7673f9a736dcbd6b21be26afae3cd91b6e3c07dc
Usman Zahid <developerusman@yahoo.com>
2025-04-17 22:36:29 +0500

    Design updates to the blog for better alignment

--- assets/javascript/dev_blog_list_fetch.js
+++ assets/javascript/dev_blog_list_fetch.js
@@ -4,21 +4,14 @@ fetch('https://dev.to/api/articles?username=usmanzahidcode')
         const container = document.getElementById('blog-posts');
         posts.forEach(post => {
             const article = document.createElement('article');
-            article.className = "flex max-w-xl flex-col items-start justify-between";
+            article.className = "flex max-w-xl gap-5 items-center justify-start";
 
             article.innerHTML = `
-                    <div class="flex items-center gap-x-4 text-sm text-gray-500">
-                        <time datetime="${post.published_timestamp}">${post.readable_publish_date}</time>
-                    </div>
-                    <div class="group relative">
-                        <h3 class="mt-3 text-lg font-semibold leading-snug text-white group-hover:text-sky-400 transition duration-300">
-                            <a href="${post.url}" target="_blank" rel="noopener">
-                                <span class="absolute inset-0"></span>
-                                ${post.title}
-                            </a>
-                        </h3>
-                    </div>
-                `;
+                <time class="text-sm text-sky-500 text-left" datetime="${post.published_timestamp}">${post.readable_publish_date}</time>
+                <h3 class="text-lg text-left font-semibold leading-snug text-white group-hover:text-sky-400 transition duration-300">
+                    <a href="${post.url}" target="_blank" rel="noopener">${post.title}</a>
+                </h3>
+            `;
 
             container.appendChild(article);
         });

ef651de90463cbe95fe5976aebbb093ba3f62799
Usman Zahid <developerusman@yahoo.com>
2025-04-19 12:19:23 +0500

    Blog listing page completed

--- assets/javascript/dev_blog_list_fetch.js
+++ assets/javascript/dev_blog_list_fetch.js
@@ -4,14 +4,14 @@ fetch('https://dev.to/api/articles?username=usmanzahidcode')
         const container = document.getElementById('blog-posts');
         posts.forEach(post => {
             const article = document.createElement('article');
-            article.className = "flex items-start gap-4";
+            article.className = "flex items-start space-x-4";
 
             article.innerHTML = `
-                <div class="w-24 flex-shrink-0 text-sm text-sky-500 text-right">
+                <div class="w-24 flex-shrink-0 text-base text-sky-500 text-right">
                     <time datetime="${post.published_timestamp}">${post.readable_publish_date}</time>
                 </div>
                 <div class="flex-1">
-                    <h3 class="text-base text-left font-semibold leading-snug hover:text-sky-500 transition duration-300">
+                    <h3 class="text-base text-left hover:text-sky-500 transition duration-300">
                         <a href="${post.url}" target="_blank" rel="noopener">${post.title}</a>
                     </h3>
                 </div>
